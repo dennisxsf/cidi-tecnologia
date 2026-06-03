7825293bf1f2331e5b8a347ed459e2121fdf844e
dennisxsf <dennisxsf@users.noreply.github.com>
2026-06-03 20:38:00 -0300

    Configura deploy no GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProduction ? "/cidi-tecnologia" : undefined,
+  assetPrefix: isProduction ? "/cidi-tecnologia/" : undefined,
 };
 
 export default nextConfig;
